e00093f5d1aa2bc4b7e8580b03b8c4117d2261e0
= <agodinezvizcardo@gmail.com>
2021-01-11 13:00:07 -0500

    fixing login box in responsive

--- resources/views/areas/index.blade.php
+++ resources/views/areas/index.blade.php
@@ -1,20 +1,11 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Transactions</title>
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-</head>
-<body>
-    <div id="app">
-        <index-areas-component 
+
+
+@extends('template.template')
+
+@section('content')
+    <index-areas-component 
             logout-route="{{ route('auth.logout') }}"
             areas-route="{{ route('areas.index') }}"
             transactions-route="{{ route('transactions.index') }}"
         />
-    </div>
-    <script src="{{ asset('js/app.js') }}"></script>
-</body>
-</html>
+@endsection
